9fd5437b9c94ff674545194d829b20e605945202
mrphilips <dav2022@gmail.com>
2018-01-09 21:09:35 +0100

    server get player position

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,27 @@
 var io = require('socket.io')();
 
 io.on('connection', function(client){
-	console.log(client);
+	console.log('CONNECT');
+	client.emit("hi", {
+		content:"Oh Hi player",
+	  	code: 300
+	});
+
+	client.on('test', (x,y,z, callback)=>{
+		console.log('TEST');
+		console.log(x+','+y+','+z);
+		console.log(callback);
+		callback("ok test");
+	});
+
+	client.on('player position', (content, callback)=>{
+		console.log('PLAYER POSITION');
+		console.log(content);
+		console.log(callback);
+		callback('ok position');
+	})
 });
+
+
 
 io.listen(3000);
