7e25030edeeda1755273f68c7c314adb49f050cc
Justin Grivon <grivon.justin@gmail.com>
2018-01-26 10:04:07 +0100

    add verb for removing trap

--- backend/src/index.js
+++ backend/src/index.js
@@ -103,6 +103,15 @@ io.on('connection', (socket) => {
     console.log(`Received verb ${Protocol.TRAP_TRIGGERED}`);
     console.log(data);
     broadcast(tables, Protocol.TRAP_TRIGGERED, data);
+    broadcast(tablets, Protocol.TRAP_TRIGGERED, data);
+  });
+
+  // REMOVE_TRAP
+  socket.on(Protocol.REMOVE_TRAP, (data) => {
+    console.log(`Received verb ${Protocol.REMOVE_TRAP}`);
+    console.log(data);
+    broadcast(tables, Protocol.REMOVE_TRAP, data);
+    broadcast(vrs, Protocol.REMOVE_TRAP, data);
   });
 
   // GAME_OVER
