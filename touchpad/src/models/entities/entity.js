3e4f24a426eeec441da8dd5919ebf4dce5e7b612
Justin Grivon <grivon.justin@gmail.com>
2018-02-09 13:47:32 +0100

    refactor + update

--- touchpad/src/models/entities/entity.js
+++ touchpad/src/models/entities/entity.js
@@ -1,6 +1,7 @@
 function Entity(name, model, x=-1, y=-1){
     this.info = model.add.sprite(x,y, name);
     this.info.anchor.setTo(0.5, 0.5);
+    this.info.visible = true;
     model.entities.push(this);
 }
 
